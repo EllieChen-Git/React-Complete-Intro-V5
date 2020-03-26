2577e8af3daa0122a9092c85176c0b24ca3551de
Ellie <elliechen.etc@gmail.com>
2020-03-26 15:17:14 +1100

    React Context: class components

--- src/Details.js
+++ src/Details.js
@@ -2,6 +2,7 @@ import React from "react";
 import pet from "@frontendmasters/pet";
 import Carousel from "./Carousel";
 import ErrorBoundary from "./ErrorBoundary";
+import ThemeContext from "./ThemeContext";
 
 class Details extends React.Component {
   // 'classProperties' syntax
@@ -36,7 +37,19 @@ class Details extends React.Component {
         <div>
           <h1>{name}</h1>
           <h2>{`${animal} - ${breed} - ${location}`}</h2>
-          <button>Adopt {name}</button>
+          <ThemeContext.Consumer>
+            {/* {themeHook => (
+              <button style={{ backgroundColoer: themeHook[0] }}>
+                Adopt {name}
+              </button>
+            )} */}
+
+            {/* Same as the above but used destructuring */}
+            {([theme]) => (
+              <button style={{ backgroundColor: theme }}>Adopt {name}</button>
+            )}
+          </ThemeContext.Consumer>
+
           <p>{description}</p>
         </div>
       </div>
